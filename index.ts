49e4bacbf8a4154df9f01cebdd51a437848e1557
andy <andrew@teller.org>
2022-07-07 19:03:24 -0400

    init

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+
+
+//export type APICall = (req: any, res: any) => any
+ 
+
+export default class Plugin { 
+
+   constructor( ) {
+     
+  }
+    
+}
---
+++ test/plugin.test.ts
@@ -0,0 +1,26 @@
+import { config, expect, should } from 'chai'
+ 
+
+  
+
+
+describe('Plugin', () => {
+ 
+  before(async () => {
+ 
+
+  })
+
+ 
+  
+  it('can test', async () => {
+ 
+    expect(true).to.eql(true)
+
+  })
+
+ 
+
+ 
+
+})
